ac70109eba00d85b453325a59d74fac785864061
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-08 17:34:01 +0200

    Fixed euclidian

--- distances/euclidean.py
+++ distances/euclidean.py
@@ -4,6 +4,6 @@ def euclidean(a:dict, b:dict)->float:
     total = 0
     aget = a.get
     bget = b.get
-    for k in (set(a) || set(b)):
+    for k in (set(a) | set(b)):
         total += (aget(k,0)-bget(k,0))**2
     return math.sqrt(total)
