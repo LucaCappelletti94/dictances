377dd82c6e14a6af70c0738f8003ed538f22933f
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-12 21:54:11 +0200

    Added seed reset

--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -1,8 +1,8 @@
 import random
-random.seed(42) # for reproducibility
 
-def create_case(n=100):
+def create_case(n=100, seed = 42):
+    random.seed(seed) # for reproducibility
     return {random.randint(0,1000):random.uniform(0,1) for i in range(n)}
 
-def create_cases(n=100):
-    return create_case(n), create_case(n)
+def create_cases(n=100, seed_1 = 42, seed_2 = 69):
+    return create_case(n, seed=seed_1), create_case(n, seed = seed_2)
