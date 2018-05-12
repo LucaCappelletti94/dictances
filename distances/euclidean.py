428838e95ac20de5bdc0ee7b5a6be35741c2fc94
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-12 21:45:19 +0200

    Now euclidean is faster

--- distances/euclidean.py
+++ distances/euclidean.py
@@ -4,6 +4,9 @@ def euclidean(a:dict, b:dict)->float:
     total = 0
     aget = a.get
     bget = b.get
-    for k in (set(a) | set(b)):
-        total += (aget(k,0)-bget(k,0))**2
+    for k in a:
+        total += (aget(k)-bget(k,0))**2
+    for k in b:
+        if k not in a:
+            total += bget(k)**2
     return math.sqrt(total)
