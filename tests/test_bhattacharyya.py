102b03b885f7548258e944016629f634dd1134d9
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-13 14:37:24 +0200

    Fixed B distance test

--- tests/test_bhattacharyya.py
+++ tests/test_bhattacharyya.py
@@ -3,4 +3,4 @@ from utils import create_cases
 
 def test_bhattacharyya():
     a, b = create_cases()
-    assert bhattacharyya(a,b) == 0.563571437813277
+    assert bhattacharyya(a,b) == 2.09088236794973
