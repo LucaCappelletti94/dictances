2fcc7d3e50efbc13c8cc6facc87dfdc90983b3b1
David Berdik <berdikd@duq.edu>
2019-10-29 22:07:19 -0400

    Although it is common practice to normalize when using Chi Square distance, it is not mandatory, and should therefore not be forced by the library implementation

--- dictances/chi_square.py
+++ dictances/chi_square.py
@@ -3,30 +3,18 @@
 def chi_square(a: dict, b: dict)->float:
     """Return the chi square distance beetween a and b."""
 
-    # Chi Square requires normalized values (sum to 1), so normalize a and b to a_norm and b_norm.
-    a_norm = dict()
-    factor = 1.0/sum(a.values())
-    for k in a:
-        a_norm[k] = a[k]*factor
-
-    b_norm = dict()
-    factor = 1.0/sum(b.values())
-    for k in b:
-        b_norm[k] = b[k]*factor
-
-    # After normalizing, do the math.
     total = 0
-    for k, aval in a_norm.items():
+    for k, aval in a.items():
         try:
-            bval = b_norm[k]
+            bval = b[k]
             total += (aval - bval) * (aval - bval) / (aval + bval)
         except KeyError:
             total += aval
 
-        for k in b_norm:
+        for k in b:
             try:
-                a_norm[k]
+                a[k]
             except KeyError:
-                total += b_norm[k]
+                total += b[k]
 
     return total
