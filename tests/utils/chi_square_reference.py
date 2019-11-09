8841c4d83b3721418e783697b1195c827279ff75
David Berdik <berdikd@duq.edu>
2019-11-08 20:33:57 -0500

    Maybe this is the correct way to handle the unit test? I don't really know.

--- tests/utils/chi_square_reference.py
+++ tests/utils/chi_square_reference.py
@@ -1,4 +1,24 @@
+import numpy as np
 from scipy.stats import chisquare
 
-def chi_square_distance(vals):
-	return chisquare(vals)[0]
+def chi_square_distance(repr1: np.ndarray, repr2: np.ndarray) -> float:
+    repr1.tolist()
+    repr2.tolist()
+    sum = 0
+
+    for x in range(max(repr1, repr2)):
+        val1 = 0
+        val2 = 0
+
+        try:
+            val1 = repr1[x]
+        except IndexError:
+            pass
+
+        try:
+            val2 = repr2[x]
+        except IndexError:
+            pass
+
+        sum += chisquare([val1,val2])[0]
+    return sum
